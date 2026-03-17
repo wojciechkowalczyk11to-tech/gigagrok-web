16d0506e436360dc40ac56b6e86feed1c514a21c
Kombinator <wojciech.kowalczyk11to@gmail.com>
2026-03-17 21:53:22 +0100

    init: Next.js config

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [{ source: "/api/:path*", destination: "/api/:path*" }]
+  }
+}
+module.exports = nextConfig
